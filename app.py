923aed07de80fea93015fd506d2230d99c1d8f13
Alexandr Ryaskov <blantyk@mail.ru>
2024-07-04 14:17:35 +0500

    Update A

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-def f(a=0, =0b):
-	print("Git good")
-	c = 10
-	return a + b * c
-
-
-res = f(1, 5)
-print(res)
+def calculate(expression):
+    """calculator best the best"""
+    Allowed = "+ - * /" 
+    if not any(sing in expression for sing in allowed):
+        raise ValueError(f"Invalid expression: {expression}")
+    for sing in allowed:
+        if sing in expression:
+            try:
+                left, right = expression.split(sing)
+                left, right = int(left), int(right)
+                return {
+                    "+": lambda x, y: x + y,
+                    "-": lambda x, y: x - y,
+                    "*": lambda x, y: x * y,
+                    "/": lambda x, y: x / y,
+                }[sing](left, right)
+            except (ValueError, TypeError):
+                raise ValueError(f"Invalid expression: {expression}")
 
